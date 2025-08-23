f8e8b7095676594f610ad6e976a116067d9c2471
raager09 <aarmaansekhon.123@gmail.com>
2025-08-22 19:07:38 -0700

    changed browser tab display

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,21 +1,27 @@
 // pages/index.tsx
 import DiscordPresence from '@/components/DiscordPresence';
-import { SocialLinks } from '@/components/SocialLinks'; // Make sure the import matches your export
+import { SocialLinks } from '@/components/SocialLinks';
 
 const Index = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center p-8">
-      {/* Main content centered vertically and horizontally */}
-      <div className="content-overlay w-full max-w-md">
-        {/* Discord Presence - Centerpiece */}
-        <div className="mb-8">
-          <DiscordPresence />
+    <>
+      <title>raager - Digital Portfolio</title>
+      <meta name="description" content="My digital portfolio and online presence" />
+      <link rel="icon" href="/favicon.ico" />
+      
+      <div className="min-h-screen flex flex-col items-center justify-center p-8">
+        {/* Main content centered vertically and horizontally */}
+        <div className="content-overlay w-full max-w-md">
+          {/* Discord Presence - Centerpiece */}
+          <div className="mb-8">
+            <DiscordPresence />
+          </div>
+          
+          {/* Social Links - Using your existing component */}
+          <SocialLinks />
         </div>
-        
-        {/* Social Links - Using your existing component */}
-        <SocialLinks />
       </div>
-    </div>
+    </>
   );
 };
 
